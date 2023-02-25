cc8cfbbba69b35c5f1d5de3368bd262f1e723c55
JW Jacobson <116485484+jwjacobson@users.noreply.github.com>
2023-02-25 12:53:00 -0500

    Improve formatting of box_print function

--- food_replicator.py
+++ food_replicator.py
@@ -3,47 +3,53 @@ import random
 
 # function for printing dict output in a box
 def box_print(x):
-    max_length = 0
-    for k, v, in daily_offering.items():
-        entry_length = len(k) + len(v) + 9
-        if entry_length > max_length:
-            max_length = entry_length
-    blank_line = "#" + " "*(max_length - 2) + "#"
-    title = "#   TODAY'S OFFERING"
+    width = 50
+    blank_line = "#" + " "*(width - 2) + "#"
+    title = "#" + "TODAY'S OFFERINGS".center(width - 2) + "#"
     print("\n")
-    print("#"*max_length)
+    print("#"*width)
     print(blank_line)
-    print(title + " "*(max_length - len(title) - 1) + "#")
+    print(title)
     print(blank_line)
+    
+    longest_key = max(daily_offering.keys(), key=len)
+
     for k, v in daily_offering.items():
-        entry_length = len(k) + len(v) + 9
-        if entry_length == max_length:
-            print(f"#   {k} - {v} #")
+        standard_entry = "#   " + f"{k}" +" "*10 + f"{v}"
+        shorter_entry = "#   " + f"{k}" +" "*10 + " "*(len(longest_key) - len(k)) +f"{v}"
+        standard_closer = " "*(width - len(standard_entry) - 1) + "#"
+        shorter_closer = " "*(width - len(shorter_entry) - 1) + "#"
+        if k == longest_key:
+            print(standard_entry + standard_closer)
         else:
-            print(f"#   {k} - {v} " + " "*(max_length - entry_length) + "#")
+            print(shorter_entry + shorter_closer)
     print(blank_line)
-    print("#"*max_length)
+    print("#"*width)
 
 
-preparations = {"canned", "boiled", "preserved", "fermented", "steamed", "gluten-free", "dried", "spiced", "fried", "frozen", "salted", "reconstituted", "vanilla-flavored"}
-foods = {"olives", "bananas", "meat", "fish", "eggs", "pasta", "broth", "onions", "berries", "shellfish", "cheese", "cow's milk", "squash"}
+
+preparations = {"canned", "boiled", "preserved", "fermented", "powdered", "gluten-free", "dehydrated", "spiced", "pickled", "frozen", "salted", "reconstituted", "gelatinated"}
+foods = {"olives", "bananas", "meat", "fish", "bird eggs", "pasta", "broth", "onions", "berries", "shellfish", "cheese", "cow's milk", "squash"}
 goods = set()
 prices = set()
 
 # Create a random combination of provided elements each time the program is run
-while len(preparations) > 3:                                #Not all elements are used each time for variety
+while len(preparations) > 5:                                #Not all elements are used each time for variety
     goods.add(preparations.pop() + " " + foods.pop())
 while len(prices) < len(goods):
-    prices.add(str(random.randint(2, 30)) + " credits")     #Generate some prices
+    prices.add(str(random.randint(10, 50)) + " credits")     #Generate some prices
 daily_offering = dict(zip(goods,prices))                    #Associate goods with prices
 
+
+def welcome():
+    welcome_txt = " Welcome to the Food Replicator! "
+    print("\n")
+    print("#"*50)
+    print(welcome_txt.center(50, "#"))
+    print("#"*50)
+
+welcome()
 box_print(daily_offering)
-
-
-# print("\n")
-# print("#"*50)
-# print("######## Welcome to the Food Replicator! #########")
-# print("#"*50)
 
 # print("\nAll food from the Food Replicator is free.\
 #     \nHowever, we do offer pricing emulation to simulate a scarcity-era 'shopping' experience.")
